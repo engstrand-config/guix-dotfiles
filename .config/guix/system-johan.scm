75684311dcf1b52288e381593b10963278970226
Johan Engstrand <johan@engstrand.nu>
2021-06-29 11:35:42 +0200

    config: pavilion, nix for johan (wip)

--- .config/guix/system-johan.scm
+++ .config/guix/system-johan.scm
@@ -1,15 +1,76 @@
-;; This is an operating system configuration generated
-;; by the graphical installer.
+(define %xorg-libinput-config
+"
+Section \"InputClass\"
+    Identifier \"Touchpads\"
+    Driver \"libinput\"
+    MatchDevicePath \"/dev/input/event*\"
+    MatchIsTouchPad \"on\"
+    Option \"DisableWhileTyping\" \"on\"
+    Option \"MiddleEmulation\" \"on\"
+    Option \"NaturalScrolling\" \"true\"
+EndSection
+Section \"InputClass\"
+    Identifier \"Keyboards\"
+    Driver \"libinput\"
+    MatchDevicePath \"/dev/input/event*\"
+    MatchIsKeyboard \"on\"
+EndSection
+")
 
-(use-modules (gnu) (nongnu packages linux))
+(define %pavilion-graphics
+"
+Section \"Device\"
+        Identifier      \"Device0\"
+        Driver          \"nvidia\"
+        VendorName      \"NVIDIA Corporation\"
+        BusID           \"PCI:1:0:0\"
+EndSection
+Section \"Device\"
+  Identifier  \"Intel Graphics\"
+  Driver      \"intel\"
+  Option      \"AccelMethod\" \"sna\"
+  Option      \"TearFree\" \"true\"
+EndSection
+")
+
+(use-modules
+	(gnu)
+        (gnu services pm)
+	(engstrand packages)
+	(nongnu packages linux)
+	(nongnu system linux-initrd))
+
+(use-service-modules nix)
+(use-package-modules package-management)
+
 (use-service-modules desktop networking ssh xorg)
+
+; Allow users in the video group to modify backlight without using sudo
+(define %backlight-udev-rule
+  (udev-rule
+    "90-backlight.rules"
+    (string-append "ACTION==\"add\", "
+		   "SUBSYSTEM==\"backlight\", "
+		   "RUN+=\"/run/current-system/profile/bin/chgrp video /sys/class/backlight/%k/brightness\"\n"
+		   "ACTION==\"add\", "
+		   "SUBSYSTEM==\"backlight\", "
+		   "RUN+=\"/run/current-system/profile/bin/chmod g+w /sys/class/backlight/%k/brightness\"\n"
+		   "ACTION==\"add\", "
+		   "SUBSYSTEM==\"leds\", "
+		   "RUN+=\"/run/current-system/profile/bin/chgrp input /sys/class/leds/%k/brightness\"\n"
+		   "ACTION==\"add\", "
+		   "SUBSYSTEM==\"leds\", "
+		   "RUN+=\"/run/current-system/profile/bin/chmod g+w /sys/class/leds/%k/brightness\"")))
 
 (operating-system
   (kernel linux)
+  (kernel-arguments (list "modprobe.blacklist=pcspkr,snd_pcsp"))
+  (initrd microcode-initrd)
   (firmware (list linux-firmware))
   (locale "en_US.utf8")
   (timezone "Europe/Stockholm")
-  (keyboard-layout (keyboard-layout "us"))
+  (keyboard-layout
+	(keyboard-layout "us,se" #:options '("grp:alt_shift_toggle" "grp_led:caps" "caps:escape")))
   (host-name "pavilion")
   (users (cons* (user-account
                   (name "johan")
@@ -19,15 +80,29 @@
                   (supplementary-groups
                     '("wheel" "netdev" "audio" "video")))
                 %base-user-accounts))
+
   (packages
     (append
-      (list (specification->package "nss-certs"))
+      (list
+	  (specification->package "nss-certs")
+	  (specification->package "engstrand-dwm")
+	  (specification->package "engstrand-st")
+	  (specification->package "engstrand-dsblocks")
+          (specification->package "nix"))
+	  ;(specification->package "engstrand-dmenu")
       %base-packages))
   (services
     (append
       (list (service gnome-desktop-service-type)
+            (service tlp-service-type
+                (tlp-configuration
+                    (cpu-scaling-governor-on-ac (list "performance"))
+                    (sched-powersave-on-bat? #t)))
+            (service nix-service-type)
+	    (udev-rules-service 'backlight %backlight-udev-rule)
             (set-xorg-configuration
               (xorg-configuration
+                (extra-config (list %xorg-libinput-config))
                 (keyboard-layout keyboard-layout))))
       %desktop-services))
   (bootloader
@@ -52,3 +127,4 @@
              (type "ext4")
              (dependencies mapped-devices))
            %base-file-systems)))
+
