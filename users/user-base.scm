87978789efecd266a4cafe4e24501babda4dc89f
Fredrik Engstrand <fredrik@engstrand.nu>
2021-07-10 21:08:59 +0200

    users: export record getters

--- users/user-base.scm
+++ users/user-base.scm
@@ -1,6 +1,13 @@
 (define-module (users user-base)
     #:use-module (srfi srfi-9)
-    #:export (system-user? make-system-user))
+    #:export (
+        system-user?
+        make-system-user
+        system-user-account
+        system-user-name
+        system-user-email
+        system-user-gpg-key
+        system-user-github))
 
 (define-record-type <system-user>
     (make-system-user account name email gpg-key github)
