f5607cb31c56fbec2843032d777055de6564d7bf
Fredrik Engstrand <fredrik@engstrand.nu>
2021-08-05 21:49:55 +0200

    systems: migrate p400s and pavilion to rde

---
+++ engstrand/systems/p400s.scm
@@ -0,0 +1,34 @@
+(define-module (engstrand systems p400s)
+               #:use-module (engstrand utils)
+               #:use-module (engstrand systems)
+               #:use-module (rde features system)
+               #:use-module (nongnu packages linux)
+               #:use-module (nongnu system linux-initrd)
+               #:use-module (gnu system file-systems)
+               #:use-module (gnu system mapped-devices))
+
+(define-public %system-p400s-swap
+               (list (uuid "2e1cafae-8f43-4938-be21-fc525250f915")))
+
+(define-public %system-p400s
+               (list
+                 (feature-kernel
+                   #:kernel linux
+                   #:kernel-arguments %engstrand-kernel-arguments)
+                 (feature-host-info
+                   #:host-name "p400s"
+                   #:timezone %engstrand-timezone
+                   #:locale %engstrand-locale)
+                 (feature-bootloader)
+                 (feature-file-systems
+                   #:file-systems
+                   (list (file-system
+                           (mount-point "/boot/efi")
+                           (device (uuid "8396-F2E9" 'fat32))
+                           (type "vfat"))
+                         (file-system
+                           (mount-point "/")
+                           (device
+                             (uuid "3d72e2f8-6474-4b99-8087-48094ed37f2b"
+                                   'ext4))
+                           (type "ext4"))))))
---
+++ engstrand/systems/pavilion.scm
@@ -0,0 +1,35 @@
+(define-module (engstrand systems pavilion)
+               #:use-module (engstrand utils)
+               #:use-module (engstrand systems)
+               #:use-module (rde features system)
+               #:use-module (nongnu packages linux)
+               #:use-module (nongnu system linux-initrd)
+               #:use-module (gnu system file-systems)
+               #:use-module (gnu system mapped-devices))
+
+(define-public %system-pavilion-swap
+               (list (uuid "62f47965-ad3e-40a9-bb5e-46e4387fa449")))
+
+(define-public %system-pavilion
+               (list
+                 (feature-kernel
+                   #:kernel linux
+                   #:kernel-arguments %engstrand-kernel-arguments)
+                 (feature-host-info
+                   #:host-name "pavilion"
+                   #:timezone %engstrand-timezone
+                   #:locale %engstrand-locale)
+                 (feature-bootloader)
+                 (feature-file-systems
+                   #:file-systems
+                   (list
+                     (file-system
+                       (mount-point "/boot/efi")
+                       (device (uuid "1ADC-28E8" 'fat32))
+                       (type "vfat"))
+                     (file-system
+                       (mount-point "/")
+                       (device
+                         (uuid "6c3ee1c8-6ee6-4142-b2bf-a370854f63e7"
+                               'ext4))
+                       (type "ext4"))))))
