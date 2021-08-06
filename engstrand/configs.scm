efd5818b7b2a5fe2a54e47bc1bd22fef4add9696
Fredrik Engstrand <fredrik@engstrand.nu>
2021-08-06 19:42:51 +0200

    fix: update dotfiles definition format and guix aliases

--- engstrand/configs.scm
+++ engstrand/configs.scm
@@ -91,19 +91,11 @@
                       "git@github.com:engstrand-config/farg.git")))
                  (feature-dotfiles
                    #:dotfiles
-                   `(("aliasrc" .
-                      ,(local-file "files/aliasrc"))
-                     ("inputrc" .
-                      ,(local-file "files/inputrc"))
-                     ("nix-channels" .
-                      ,(local-file "files/nix-channels"))
-                     ("config/guix/channels.scm" .
-                      ,(local-file "../channels.scm"))
-                     ("config/dunst/dunstrc" .
-                      ,(local-file "files/config/dunst/dunstrc"))
-                     ("config/nvim/init.vim" .
-                      ,(local-file "files/config/nvim/init.vim"))
-                     ("config/nvim/autoload/plug.vim" .
-                      ,(local-file "files/config/nvim/autoload/plug.vim"))
-                     ("config/picom/picom.conf" .
-                      ,(local-file "files/config/picom/picom.conf"))))))
+                   `(("aliasrc" ,(local-file "files/aliasrc"))
+                     ("inputrc" ,(local-file "files/inputrc"))
+                     ("nix-channels" ,(local-file "files/nix-channels"))
+                     ("config/guix/channels.scm" ,(local-file "channels.scm"))
+                     ("config/dunst/dunstrc" ,(local-file "files/config/dunst/dunstrc"))
+                     ("config/nvim/init.vim" ,(local-file "files/config/nvim/init.vim"))
+                     ("config/nvim/autoload/plug.vim" ,(local-file "files/config/nvim/autoload/plug.vim"))
+                     ("config/picom/picom.conf" ,(local-file "files/config/picom/picom.conf"))))))
