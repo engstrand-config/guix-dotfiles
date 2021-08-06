095ac697f717d8dd5ce8a41ba3d710cd4ba30580
Fredrik Engstrand <fredrik@engstrand.nu>
2021-08-06 11:43:55 +0200

    features: add virtualization

--- engstrand/features/virtualization.scm
+++ engstrand/features/virtualization.scm
@@ -1,13 +1,28 @@
-(define-module (engstrand features virtualization))
+(define-module (engstrand features virtualization)
+               #:use-module (rde features)
+               #:use-module (rde features predicates)
+               #:use-module (gnu services)
+               #:use-module (gnu services virtualization)
+               #:export (feature-virtualization))
 
 ; TODO: Add groups to users
 ;       (if virt? (list "libvirt" "kvm") '()))))
 
-; TODO: Add packages
-; TODO: Add services
-;       (if virt?
-;           (list
-;             (service virtlog-service-type)
-;             (service libvirt-service-type
-;                      (libvirt-configuration (unix-sock-group "libvirt"))))
-;           '())
+(define* (feature-virtualization
+           #:key
+           (unix-sock-group "libvirt"))
+         "Setup system virtualization."
+
+         (ensure-pred string? unix-sock-group)
+
+         (define (get-system-services config)
+           "Return a list of system services required for virtualization."
+           (list
+            (service virtlog-service-type)
+            (service libvirt-service-type
+                     (libvirt-configuration
+                       (unix-sock-group unix-sock-group)))))
+
+         (feature
+           (name 'virtualization)
+           (system-services-getter get-system-services)))
