9b87309bc8fd3fa6304544a22da3c58d9b751f7b
Fredrik Engstrand <fredrik@engstrand.nu>
2021-08-06 11:27:44 +0200

    features: add xorg dwm

--- engstrand/features/xorg.scm
+++ engstrand/features/xorg.scm
@@ -1,4 +1,12 @@
-(define-module (engstrand features xorg))
+(define-module (engstrand features xorg)
+               #:use-module (rde features)
+               #:use-module (rde features predicates)
+               #:use-module (engstrand packages engstrand-utils)
+               #:use-module (guix gexp)
+               #:use-module (gnu services)
+               #:use-module (gnu home-services)
+               #:use-module (gnu home-services base)
+               #:export (feature-xorg-dwm))
 
 (define %xorg-libinput-config
   "
@@ -18,24 +26,53 @@
   EndSection
   ")
 
-; TODO: Add packages:
-;       "chili-sddm-theme" "engstrand-dwm" "engstrand-dmenu" "engstrand-dsblocks" "engstrand-st"
+(define %base-xorg-dwm-system-packages
+  '("chili-sddm-theme" "engstrand-dwm"))
 
-; TODO: Modify desktop services:
-;       (modify-services %desktop-services
-;                        (delete gdm-service-type))))
+(define %base-xorg-dwm-home-packages
+  '("engstrand-dmenu" "engstrand-dsblocks" "engstrand-st"))
 
-; TODO: Add service:
-;       (service sddm-service-type
-;                (sddm-configuration
-;                  (theme "chili")
-;                  (display-server "wayland")
-;                  (xorg-configuration
-;                    (xorg-configuration
-;                      (keyboard-layout keyboard-layout)
-;                      (extra-config (append (list %xorg-libinput-config) xorg-extra)))))))
+(define* (feature-xorg-dwm
+           #:key
+           (sddm-theme "chili")
+           (extra-config '())
+           (base-extra-config (list %xorg-libinput-config)))
+         "Sets up xorg with SDDM (display manager), dwm, dsblocks, dmenu and st."
 
-; TODO: Add startup script:
-;       (simple-service
-;         'bootstrap home-run-on-first-login-service-type
-;         #~(system* #$(file-append engstrand-utils "/bin/bootstrap"))))
+         (ensure-pred string? sddm-theme)
+         (ensure-pred list-of-strings? extra-config)
+         (ensure-pred list-of-strings? base-extra-config)
+
+         (define (get-home-services config)
+           "Return a list of home-services required by xorg and dwm."
+           (list
+             (simple-service
+               'add-xorg-dwm-home-packages-to-profile
+               home-profile-service-type
+               %base-xorg-dwm-home-packages)
+             ; TODO: Consider making this script inline to skip the extra package depenendcy
+             (simple-service
+               'xorg-dwm-bootstrap home-run-on-first-login-service-type
+               #~(system* #$(file-append engstrand-utils "/bin/bootstrap")))))
+
+         (define (get-system-services config)
+           "Return a list of system services required by xorg and dwm."
+           (require-value 'desktop-services config)
+           (require-value 'keyboard-layout config)
+           (list
+             (simple-service
+               'add-xorg-dwm-system-packages-to-profile
+               profile-service-type
+               %base-xorg-dwm-system-packages)
+             (service sddm-service-type
+                      (sddm-configuration
+                        (theme sddm-theme)
+                        (xorg-configuration
+                          (xorg-configuration
+                            (keyboard-layout (get-value 'keyboard-layout config)
+                                             (extra-config (append extra-config
+                                                                   base-extra-config)))))))))
+         (feature
+           (name 'xorg-dwm)
+           (home-services-getter get-home-services)
+           (system-services-getter get-system-services)))
