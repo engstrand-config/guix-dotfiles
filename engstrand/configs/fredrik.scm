aa0af5a9afd887f320e9a24017365bb3c5703da8
Fredrik Engstrand <fredrik@engstrand.nu>
2021-08-06 19:45:35 +0200

    configs: use pinentry gtk2 and custom zsh

--- engstrand/configs/fredrik.scm
+++ engstrand/configs/fredrik.scm
@@ -8,7 +8,17 @@
                #:use-module (engstrand configs)
                #:use-module (engstrand features nix)
                #:use-module (engstrand features state)
+               #:use-module (engstrand features xorg)
                #:use-module (engstrand features wayland))
+
+; TODO: Remove xorg feature again.
+(define %xorg-amdgpu-config
+  "Section \"Device\"
+  Identifier  \"AMD\"
+  Driver      \"amdgpu\"
+  Option      \"TearFree\" \"true\"
+  Option      \"Backlight\" \"amdgpu_bl0\"
+  EndSection")
 
 (define-public %user-features
                (list
@@ -16,8 +26,16 @@
                    #:user-name "fredrik"
                    #:full-name "Fredrik Engstrand"
                    #:email "fredrik@engstrand.nu")
+                 ; TODO: Add custom gnupg feature. We should be using pinentry-bemenu instead.
+                 ;       However, home-gnupg-service will always append "/bin/pinentry" to the
+                 ;       pinentry exectuable in ~/.gnupg/gpg-agent.conf. pinentry-bemenu does not
+                 ;       have such an exectuable. The bemenu exectuable is called "pinentry-bemenu".
+                 ;
+                 ;       This leaves us with two options; add support for it in the home-service,
+                 ;       or transform the pinentry-bemenu package and move the pinentry exectuable to "/bin/pinentry".
                  (feature-gnupg
                    #:gpg-primary-key "C9BEB8A04458FDDF12681B39029D8EB77E18D68C"
+                   #:pinentry-flavor 'gtk2
                    #:gpg-smart-card? #f)
                  (feature-git
                    #:sign-commits? #t)
@@ -26,6 +44,8 @@
                    `(("repos/pywalfox" . "git@github.com:frewacom/pywalfox.git")
                      ("repos/pywalfox-native" . "git@github.com:frewacom/pywalfox-native.git")))
                  (feature-nix)
+                 (feature-xorg-dwm
+                   #:extra-config (list %xorg-amdgpu-config))
                  (feature-wayland-dwl-guile
                    #:dwl-guile-configuration
                    (home-dwl-guile-configuration
