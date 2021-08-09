d58a76eef2b5b3f0c7cacb9d93ff85e0865ffca0
Fredrik Engstrand <fredrik@engstrand.nu>
2021-08-09 13:20:31 +0200

    features: add obs

--- engstrand/features/video.scm
+++ engstrand/features/video.scm
@@ -1,11 +1,13 @@
 (define-module (engstrand features video)
                #:use-module (rde features)
                #:use-module (rde features predicates)
+               #:use-module (gnu services)
                #:use-module (gnu home-services)
                #:use-module (gnu home-services video)
                #:use-module (engstrand utils)
                #:export (
                          feature-mpv
+                         feature-obs
                          %engstrand-mpv-configuration))
 
 ; Allows each user to easily extend a shared default configuration
@@ -25,3 +27,23 @@
          (feature
            (name 'mpv)
            (home-services-getter get-home-services)))
+
+(define* (feature-obs
+           #:key
+           (wayland? #t))
+           "Setup OBS, the Open Broadcaster Software."
+
+           (define (get-home-services config)
+             "Return a list of home services required by OBS."
+             (list
+               (simple-service
+                 'add-obs-home-packages-to-profile
+                 home-profile-service-type
+                 (let ((base-packages '("obs")))
+                   (pkgs (if wayland?
+                             (cons "obs-wlrobs" base-packages)
+                             base-packages))))))
+
+           (feature
+             (name 'obs)
+             (home-services-getter get-home-services)))
