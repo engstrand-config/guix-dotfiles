73dcd826c6ab3f08bcb33ae09fcf797686374020
Fredrik Engstrand <fredrik@engstrand.nu>
2021-08-11 20:48:21 +0200

    packages/wayland: add wbg

---
+++ engstrand/packages/wayland.scm
@@ -0,0 +1,49 @@
+(define-module (engstrand packages wayland)
+               #:use-module (guix packages)
+               #:use-module (guix git-download)
+               #:use-module (guix build-system meson)
+               #:use-module (gnu packages)
+               #:use-module (gnu packages wm)
+               #:use-module (gnu packages gcc)
+               #:use-module (gnu packages image)
+               #:use-module (gnu packages xdisorg)
+               #:use-module (gnu packages pkg-config)
+               #:use-module (gnu packages build-tools)
+               #:use-module (gnu packages freedesktop)
+               #:use-module (gnu packages datastructures)
+               #:use-module ((guix licenses) #:prefix license:))
+
+(define-public wbg
+               (package
+                 (name "wbg")
+                 (version "1.0.2")
+                 (source
+                   (origin
+                     (method git-fetch)
+                     (uri (git-reference
+                            (url "https://codeberg.org/dnkl/wbg")
+                            (commit version)))
+                     (file-name (git-file-name name version))
+                     (sha256
+                       (base32
+                         "182cyp97lxwxl5r6f25irvm62ii0j1knmpwlpwa1w00j2xchx89w"))))
+                 (build-system meson-build-system)
+                 (arguments
+                   `( #:meson ,meson-0.55
+                      #:build-type "release"))
+                 (native-inputs
+                   `(("pkg-config" ,pkg-config)
+                     ("wayland-protocols" ,wayland-protocols)
+                     ("gcc" ,gcc-10)
+                     ("tllist" ,tllist)))
+                 (inputs
+                   `(("wlroots" ,wlroots)
+                     ("wayland" ,wayland)
+                     ("pixman" ,pixman)
+                     ("libpng" ,libpng)
+                     ("libjpeg" ,libjpeg-turbo)))
+                 (license license:expat)
+                 (home-page "https://codeberg.org/dnkl/wbg")
+                 (synopsis "Super simple wallpaper application for Wayland compositors")
+                 (description "Super simple wallpaper application for
+                              Wayland compositors implementing the layer-shell protocol.")))
