2fec4e841ff942744431adb60085f010f3c66a14
Fredrik Engstrand <fredrik@engstrand.nu>
2021-08-12 20:19:27 +0200

    features: add imv

---
+++ engstrand/features/utils.scm
@@ -0,0 +1,24 @@
+(define-module (engstrand features utils)
+               #:use-module (rde features)
+               #:use-module (rde features predicates)
+               #:use-module (gnu services)
+               #:use-module (gnu home-services)
+               #:use-module (engstrand utils)
+               #:export (feature-imv))
+
+(define* (feature-imv)
+         "Setup imv, an image viewer for X11 and Wayland."
+
+         ; TODO: Add configuration in Guile
+
+         (define (get-home-services config)
+           "Return a list of home services required by imv"
+           (list
+             (simple-service
+               'add-imv-home-packages-to-profile
+               home-profile-service-type
+               (pkgs '("imv")))))
+
+         (feature
+           (name 'imv)
+           (home-services-getter get-home-services)))
