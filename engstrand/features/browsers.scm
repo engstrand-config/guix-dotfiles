199f5af515fcf365ca886858ebacd41ca3d80965
Fredrik Engstrand <fredrik@engstrand.nu>
2021-08-12 21:27:30 +0200

    features: add firefox

--- engstrand/features/browsers.scm
+++ engstrand/features/browsers.scm
@@ -4,17 +4,61 @@
                #:use-module (guix gexp)
                #:use-module (gnu services)
                #:use-module (gnu home-services)
+               #:use-module (nongnu packages mozilla)
                #:use-module (dwl-guile utils)
                #:use-module (dwl-guile home-service)
                #:use-module (dwl-guile configuration)
                #:use-module (gnu packages web-browsers)
                #:use-module (engstrand utils)
-               #:export (feature-qutebrowser))
+               #:export (
+                         feature-qutebrowser
+                         feature-firefox))
 
-; TODO: Add firefox feature?
-;       The problem with installing it from Guix is that it will almost always
-;       require you to compile it yourself (which takes a huge amount of time).
-;       You can skip the compilation step if you install it from nix.
+; TODO: Complete this feature. Currently, it is only used to add a dwl keybinding.
+(define* (feature-firefox
+           #:key
+           (open-key "w")
+           (open-modifiers '(SUPER SHIFT))
+           (spawn-parameters '("firefox"))
+           ; (spawn-parameters `(,(file-append firefox "/bin/firefox")))
+           (add-keybindings? #t))
+         "Setup Firefox."
+
+         (ensure-pred keycode? open-key)
+         (ensure-pred list-of-modifiers? open-modifiers)
+         (ensure-pred start-parameters? spawn-parameters)
+         (ensure-pred boolean? add-keybindings?)
+
+         (define (get-home-services config)
+           "Return a list of home services required by Firefox."
+           (make-service-list
+             ; How do we want to install Firefox?
+             ; Installing it from nongnu will require you to manually compile it,
+             ; which takes a huge amount of time.
+             ; (simple-service
+             ;   'add-firefox-home-packages-to-profile
+             ;   home-profile-service-type
+             ;   (list firefox))
+             (when (and add-keybindings? (get-value 'dwl-guile config))
+               (simple-service
+                 'add-firefox-dwl-keybindings
+                 home-dwl-guile-service-type
+                 (modify-dwl-guile-config
+                   (config =>
+                           (dwl-config
+                             (inherit config)
+                             (keys
+                               (append
+                                 (list
+                                   (dwl-key
+                                     (modifiers open-modifiers)
+                                     (key open-key)
+                                     (action `(dwl:spawn ,spawn-parameters))))
+                                 (dwl-config-keys config))))))))))
+
+         (feature
+           (name 'firefox)
+           (home-services-getter get-home-services)))
 
 (define* (feature-qutebrowser
            #:key
