7275ecabce041814786e266f224052993513e849
Fredrik Engstrand <fredrik@engstrand.nu>
2021-09-10 13:08:15 +0200

    fix(temp): add guix home to guile load path

--- engstrand/systems.scm
+++ engstrand/systems.scm
@@ -1,5 +1,6 @@
 (define-module (engstrand systems)
                #:use-module (rde features)
+               #:use-module (rde features base)
                #:use-module (rde features system)
                #:use-module (rde features keyboard)
                #:use-module (gnu system)
@@ -54,5 +55,18 @@
 
 (define-public %engstrand-system-base-features
                (list
+                 (feature-custom-services
+                   #:home-services
+                   (list
+                     ;; TODO: Remove it once upstreamed.
+                     ((@ (gnu services) simple-service)
+                      'make-guix-aware-of-guix-home-subcomand
+                      (@ (gnu home-services) home-environment-variables-service-type)
+                      '(("GUILE_LOAD_PATH" .
+                         "$XDG_CONFIG_HOME/guix/current/share/guile/site/3.0\
+                         :$GUILE_LOAD_PATH")
+                        ("GUILE_LOAD_COMPILED_PATH" .
+                         "$XDG_CONFIG_HOME/guix/current/lib/guile/3.0/site-ccache\
+                         :$GUILE_LOAD_COMPILED_PATH")))))
                  (feature-keyboard
                    #:keyboard-layout %engstrand-keyboard-layout)))
