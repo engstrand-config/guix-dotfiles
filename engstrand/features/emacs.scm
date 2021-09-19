57d7274a62f3130ee05be503b89fb2d60b2a52fe
Johan Engstrand <johan@engstrand.nu>
2021-09-19 19:44:26 +0200

    features: first step towards emacs feature

--- engstrand/features/emacs.scm
+++ engstrand/features/emacs.scm
@@ -1,41 +1,72 @@
-(define-module (engstrand features emacs))
+(define-module (engstrand features emacs)
+               #:use-module (engstrand utils)
+               #:use-module (gnu home-services)
+               #:use-module (gnu services)
+               #:use-module (flat packages emacs)
+               #:use-module (gnu packages emacs)
+               #:use-module (gnu packages emacs-xyz)
+               #:use-module (rde features)
+               #:use-module (rde features emacs)
+               #:export (%engstrand-emacs-base-features feature-emacs-guix))
 
-; TODO: Integrate with rde emacs features
-;       (service home-emacs-service-type
-;                (home-emacs-configuration
-;                  (package emacs-pgtk-native-comp)
-;                  ; (rebuild-elisp-packages? #t)
-;                  (xdg-flavor? #t)
-;                  (server-mode? #t)
-;                  (elisp-packages
-;                    (list
-;                      emacs-use-package
-;                      emacs-evil
-;                      emacs-guix
-;                      emacs-doom-modeline
-;                      emacs-geiser
-;                      emacs-geiser-guile
-;                      ))
-;                  (init-el
-;                    '(
-;                      (package-initialize)
-;                      (eval-when-compile
-;                        (add-to-list 'load-path
-;                                     (expand-file-name "~/.guix-home/profile/share/emacs/site-lisp/use-package-2.4.1"))
-;                        (require 'use-package))
-;                      (use-package evil
-;                                   :init
-;                                   (setq evil-want-integration t)
-;                                   (setq evil-want-keybinding nil)
-;                                   (setq evil-want-C-u-scroll t)
-;                                   (setq evil-want-C-i-jump nil)
-;                                   :hook (evil-mode . rune/evil-hook)
-;                                   :config
-;                                   (evil-mode 1)
-;                                   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-;                                   (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join))
-;                      ; (use-package doom-modeline
-;                      ;              :ensure t
-;                      ;              :init (doom-modeline-mode 1)
-;                      ;              :custom ((doom-modeline-height 15)))
-;                      ))))
+(define* (feature-emacs-guix)
+         "Add the emacs-guix package."
+
+         (define (get-home-services config)
+           (list
+             (simple-service
+               'add-emacs-home-packages-to-profile
+               home-profile-service-type
+               (pkgs '("emacs-guix")))))
+
+         (feature
+           (name 'emacs-guix)
+           (home-services-getter get-home-services)))
+
+
+(define %engstrand-emacs-base-features
+  (list
+    (feature-emacs
+      ; #:package emacs-pgtk-native-comp
+      #:package emacs-next-pgtk
+      #:additional-elisp-packages (list emacs-evil emacs-evil-collection emacs-doom-themes emacs-doom-modeline emacs-geiser emacs-geiser-guile)
+      #:extra-config '((evil-mode 1)
+                       (load-theme 'doom-one)
+                       (fringe-mode '(0 . 0))
+                       ; (evil-collection-init)
+                       ))
+    (feature-emacs-guix)
+    (feature-emacs-appearance
+      #:margin 8)
+    (feature-emacs-monocle)
+    (feature-emacs-dired)
+    (feature-emacs-faces)
+    (feature-emacs-completion)
+    (feature-emacs-project)
+    (feature-emacs-perspective)
+    (feature-emacs-input-methods)
+    (feature-emacs-which-key)
+    (feature-emacs-keycast)
+    (feature-emacs-git)
+    (feature-emacs-pdf-tools)
+    (feature-emacs-org)
+    (feature-emacs-org-roam
+      #:org-roam-directory "~/roam/")))
+
+;                  (require 'use-package))
+;                (use-package evil
+;                             :init
+;                             (setq evil-want-integration t)
+;                             (setq evil-want-keybinding nil)
+;                             (setq evil-want-C-u-scroll t)
+;                             (setq evil-want-C-i-jump nil)
+;                             :hook (evil-mode . rune/evil-hook)
+;                             :config
+;                             (evil-mode 1)
+;                             (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+;                             (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join))
+;                ; (use-package doom-modeline
+;                ;              :ensure t
+;                ;              :init (doom-modeline-mode 1)
+;                ;              :custom ((doom-modeline-height 15)))
+;                ))))
