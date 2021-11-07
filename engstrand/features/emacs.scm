bdd9ec04209a2f2c5ef5be735553f1f82db2d190
Johan Engstrand <johan@engstrand.nu>
2021-11-07 18:07:25 +0100

    features/emacs: line numbers, remove unneeded pks

--- engstrand/features/emacs.scm
+++ engstrand/features/emacs.scm
@@ -9,6 +9,7 @@
                #:use-module (rde features emacs)
                #:export (%engstrand-emacs-base-features feature-emacs-guix))
 
+
 (define* (feature-emacs-guix)
          "Add the emacs-guix package."
 
@@ -23,50 +24,32 @@
            (name 'emacs-guix)
            (home-services-getter get-home-services)))
 
-
 (define %engstrand-emacs-base-features
   (list
-    (feature-emacs
-      ; #:package emacs-pgtk-native-comp
-      #:package emacs-next-pgtk
-      #:additional-elisp-packages (list emacs-evil emacs-evil-collection emacs-doom-themes emacs-doom-modeline emacs-geiser emacs-geiser-guile)
-      #:extra-config '((evil-mode 1)
-                       (load-theme 'doom-one)
-                       (fringe-mode '(0 . 0))
-                       ; (evil-collection-init)
-                       ))
-    (feature-emacs-guix)
-    (feature-emacs-appearance
-      #:margin 8)
-    (feature-emacs-monocle)
-    (feature-emacs-dired)
-    (feature-emacs-faces)
-    (feature-emacs-completion)
-    (feature-emacs-project)
-    (feature-emacs-perspective)
-    (feature-emacs-input-methods)
-    (feature-emacs-which-key)
-    (feature-emacs-keycast)
-    (feature-emacs-git)
-    (feature-emacs-pdf-tools)
-    (feature-emacs-org)
-    (feature-emacs-org-roam
-      #:org-roam-directory "~/roam/")))
-
-;                  (require 'use-package))
-;                (use-package evil
-;                             :init
-;                             (setq evil-want-integration t)
-;                             (setq evil-want-keybinding nil)
-;                             (setq evil-want-C-u-scroll t)
-;                             (setq evil-want-C-i-jump nil)
-;                             :hook (evil-mode . rune/evil-hook)
-;                             :config
-;                             (evil-mode 1)
-;                             (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-;                             (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join))
-;                ; (use-package doom-modeline
-;                ;              :ensure t
-;                ;              :init (doom-modeline-mode 1)
-;                ;              :custom ((doom-modeline-height 15)))
-;                ))))
+   (feature-emacs
+    #:package emacs-next-pgtk
+    #:additional-elisp-packages (list emacs-evil emacs-evil-collection emacs-geiser emacs-geiser-guile)
+    #:extra-config '((evil-mode 1)
+                     (fringe-mode '(0 . 0))
+                     (global-display-line-numbers-mode)
+                     (setq display-line-numbers 'relative)
+                     (define-key key-translation-map (kbd "ESC") (kbd "C-g"))
+                                        ; (evil-collection-init)
+                     ))
+   (feature-emacs-guix)
+   (feature-emacs-appearance
+    #:margin 8)
+   (feature-emacs-monocle)
+   (feature-emacs-dired)
+   (feature-emacs-faces)
+   (feature-emacs-completion)
+   (feature-emacs-project)
+   (feature-emacs-perspective)
+   (feature-emacs-input-methods)
+   (feature-emacs-which-key)
+   (feature-emacs-git)
+   (feature-emacs-keycast)
+   (feature-emacs-pdf-tools)
+   (feature-emacs-org)
+   (feature-emacs-org-roam
+    #:org-roam-directory "~/roam/"))) ; TODO: add roam dir in guix home
