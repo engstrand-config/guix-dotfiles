01301acf000b3ce04ee2a1045a97d2235d6fae32
Fredrik Engstrand <fredrik@engstrand.nu>
2021-11-16 15:09:52 +0100

    packages/python: add python-sounddevice

--- engstrand/packages/python.scm
+++ engstrand/packages/python.scm
@@ -1,10 +1,12 @@
 (define-module (engstrand packages python)
+               #:use-module (gnu packages libffi)
                #:use-module (guix packages)
                #:use-module (guix download)
                #:use-module (guix build-system python)
-               #:use-module (guix licenses))
+               #:use-module ((guix licenses) #:prefix license:))
 
-(define-public python-pywalfox
+(define-public
+  python-pywalfox
   (package
     (name "python-pywalfox")
     (version "2.7.3")
@@ -23,3 +25,21 @@
     (description
       "Native app used alongside the Pywalfox browser extension")
     (license #f)))
+
+(define-public
+  python-sounddevice
+  (package
+    (name "python-sounddevice")
+    (version "0.4.3")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (pypi-uri "sounddevice" version))
+        (sha256
+          (base32 "083crfkrx0y9gqybqcixzhrp7swqjr78nrmz9r6aqpxncxs7lrpi"))))
+    (build-system python-build-system)
+    (propagated-inputs `(("python-cffi" ,python-cffi)))
+    (home-page "http://python-sounddevice.readthedocs.io/")
+    (synopsis "Play and Record Sound with Python")
+    (description "Play and Record Sound with Python")
+    (license license:expat)))
