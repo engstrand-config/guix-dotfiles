300add6f126ca3c56016a78318c3be2fe042c52a
Fredrik Engstrand <fredrik@engstrand.nu>
2021-12-21 11:25:38 +0100

    packages/browser: downgrade qutebrowser to 2.3.1
    Version 2.4.0 has issues with rendering on most pages. Uncertain what is causing this,
    but it works just fine in previous versions.

--- engstrand/packages/browsers.scm
+++ engstrand/packages/browsers.scm
@@ -1,17 +1,28 @@
 (define-module (engstrand packages browsers)
-    #:use-module (guix gexp)
-    #:use-module (guix utils)
-    #:use-module (guix packages)
-    #:use-module (gnu packages web-browsers))
+               #:use-module (guix gexp)
+               #:use-module (guix utils)
+               #:use-module (guix packages)
+               #:use-module (gnu packages web-browsers))
 
 ; This package is the same as the default qutebrowser package in Guix,
 ; but adds another phase that copies all official userscripts into share/.
 ;
 ;; You can find all available userscripts here:
 ; @url{https://github.com/qutebrowser/qutebrowser/tree/master/misc/userscripts}.
-(define-public qutebrowser-with-scripts
+(define-public
+  qutebrowser-with-scripts
   (package
+    (name "qutebrowser-with-scripts")
     (inherit qutebrowser)
+    (version "2.3.1")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (string-append "https://github.com/qutebrowser/"
+                            "qutebrowser/releases/download/v" version "/"
+                            "qutebrowser-" version ".tar.gz"))
+        (sha256
+          (base32 "05n64mw9lzzxpxr7lhakbkm9ir3x8p0rwk6vbbg01aqg5iaanyj0"))))
     (arguments
       `(,@(substitute-keyword-arguments
             (package-arguments qutebrowser)
