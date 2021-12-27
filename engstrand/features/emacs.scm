e74bdec8899037307ba5005f91876fd95e01e385
Johan Engstrand <johan@engstrand.nu>
2021-12-27 20:37:37 +0100

    features/emacs: modular evil feature, various tweaks

--- engstrand/features/emacs.scm
+++ engstrand/features/emacs.scm
@@ -7,35 +7,9 @@
   #:use-module (gnu packages emacs-xyz)
   #:use-module (rde features)
   #:use-module (rde features emacs)
-  #:export (feature-emacs-evil
+  #:export (feature-emacs-corfu
+            feature-emacs-evil
             %engstrand-emacs-base-features))
-
-;; (define* (feature-emacs-company
-;;           ;; #:key
-;;           ;; ()
-;;           )
-;;   "Add and configure Company completion for Emacs."
-;;   (define emacs-f-name 'company)
-;;   (define f-name (symbol-append 'emacs- emacs-f-name))
-
-;;   (define (get-home-services config)
-;;     (list
-;;      (elisp-configuration-service
-;;       emacs-f-name
-;;       `((require 'company)
-;;         (require 'company-dabbrev-code)
-;;         (add-hook 'after-init-hook 'global-company-mode)
-;;         (setq company-idle-delay 0)
-;;         (setq company-minimum-prefix-length 1)
-;;         (setq company-selection-wrap-around t)
-;;         (setq company-backend 'company-dabbrev-code)
-;;         (company-tng-configure-default))
-;;       #:elisp-packages (list
-;;                         emacs-company))))
-;;   (feature
-;;    (name f-name)
-;;    (values `((,f-name . #t)))
-;;    (home-services-getter get-home-services)))
 
 (define* (feature-emacs-corfu
           ;; #:key
@@ -83,67 +57,25 @@
               modus-themes-mixed-fonts t
               modus-themes-subtle-line-numbers t
               modus-themes-intense-markup t
-
-              ;; Options for `modus-themes-lang-checkers' are either nil (the
-              ;; default), or a list of properties that may include any of those
-              ;; symbols: `straight-underline', `text-also', `background',
-              ;; `intense' OR `faint'.
               modus-themes-lang-checkers nil
-
-              ;; Options for `modus-themes-mode-line' are either nil, or a list
-              ;; that can combine any of `3d' OR `moody', `borderless',
-              ;; `accented', and a natural number for extra padding
               modus-themes-mode-line '(borderless)
-
-              ;; Options for `modus-themes-syntax' are either nil (the default),
-              ;; or a list of properties that may include any of those symbols:
-              ;; `faint', `yellow-comments', `green-strings', `alt-syntax'
               modus-themes-syntax nil
-
-              ;; Options for `modus-themes-hl-line' are either nil (the default),
-              ;; or a list of properties that may include any of those symbols:
-              ;; `accented', `underline', `intense'
               modus-themes-hl-line '(underline intense)
-
-              ;; Options for `modus-themes-paren-match' are either nil (the
-              ;; default), or a list of properties that may include any of those
-              ;; symbols: `bold', `intense', `underline'
               modus-themes-paren-match nil
-
-              ;; Options for `modus-themes-links' are either nil (the default),
-              ;; or a list of properties that may include any of those symbols:
-              ;; `neutral-underline' OR `no-underline', `faint' OR `no-color',
-              ;; `bold', `italic', `background'
               modus-themes-links nil
-
-              ;; Options for `modus-themes-prompts' are either nil (the
-              ;; default), or a list of properties that may include any of those
-              ;; symbols: `background', `bold', `gray', `intense', `italic'
               modus-themes-prompts nil
-
-              modus-themes-mail-citations 'faint ; {nil,'faint,'monochrome}
-
-              ;; Options for `modus-themes-region' are either nil (the default),
-              ;; or a list of properties that may include any of those symbols:
-              ;; `no-extend', `bg-only', `accented'
+              modus-themes-mail-citations 'faint
               modus-themes-region '(bg-only accented)
-
-              ;; Options for `modus-themes-diffs': nil, 'desaturated, 'bg-only
               modus-themes-diffs 'nil
-
-              modus-themes-org-blocks 'gray-background ; {nil,'gray-background,'tinted-background}
-
-              modus-themes-org-agenda ; this is an alist: read the manual or its doc string
-              '((header-block . (variable-pitch 1.3))
-                (header-date . (grayscale workaholic bold-today 1.1))
-                (event . (accented varied))
-                (scheduled . uniform)
-                (habit . traffic-light))
-
-              modus-themes-headings ; this is an alist: read the manual or its doc string
-              '((1 . (background variable-pitch 1.3))
-                (2 . (rainbow overline 1.1))
-                (t . (semibold))))
+              modus-themes-org-blocks 'gray-background
+              modus-themes-org-agenda '((header-block . (variable-pitch 1.3))
+                                        (header-date . (grayscale workaholic bold-today 1.1))
+                                        (event . (accented varied))
+                                        (scheduled . uniform)
+                                        (habit . traffic-light))
+              modus-themes-headings '((1 . (background variable-pitch 1.3))
+                                      (2 . (rainbow overline 1.1))
+                                      (t . (semibold))))
         (modus-themes-load-themes))
       #:elisp-packages (list
                         emacs-modus-themes))))
@@ -155,11 +87,16 @@
 
 (define* (feature-emacs-evil
           #:key
-          (evil-collection? #t)
-          (evil-surround? #t))
+          (insert-state-message? #f)
+          (leader? #t)
+          (commentary? #t)
+          (collection? #t)
+          (surround? #t))
   "Add and configure evil-mode for Emacs."
-  (ensure-pred boolean? evil-collection?)
-  (ensure-pred boolean? evil-surround?)
+  (ensure-pred boolean? insert-state-message?)
+  (ensure-pred boolean? leader?)
+  (ensure-pred boolean? collection?)
+  (ensure-pred boolean? surround?)
   (define emacs-f-name 'evil)
   (define f-name (symbol-append 'emacs- emacs-f-name))
 
@@ -167,27 +104,52 @@
     (list
      (elisp-configuration-service
       emacs-f-name
-      `((setq evil-want-keybinding nil)
-        (setq evil-want-C-u-scroll t)
-        (setq evil-toggle-key "C-`")
-        (require 'evil)
-        (evil-mode 1)
-        (when (require 'evil-collection nil t)
-          (evil-collection-init))
-        (require 'evil-commentary)
-        (evil-commentary-mode)
-        (require 'evil-surround)
-        (global-evil-surround-mode 1)
-        ;; Make the Escape key behave more nicely for evil-mode
-	(global-set-key (kbd "<escape>") 'keyboard-quit)
+      `(;; Make the Escape key behave more nicely for evil-mode
+        (global-set-key (kbd "<escape>") 'keyboard-quit)
         (define-key query-replace-map (kbd "<escape>") 'quit)
-
-        (setq evil-insert-state-message nil))
+        ;; Required by the additional packages... should we toggle this?
+        (setq evil-want-keybinding nil)
+        ;; Hide ``-- INSERT --'' message
+        ,@(if insert-state-message?
+              `((setq evil-insert-state-message nil))
+              '())
+        ;; Use C-u to scroll up
+        (setq evil-want-C-u-scroll t)
+        ;; The packages below must be loaded and configured in a certain order
+        (require 'evil)
+        ,@(if leader?
+              `((require 'evil-leader)
+                (global-evil-leader-mode)
+                (evil-leader/set-leader "<SPC>")
+                (evil-leader/set-key
+                 "<SPC>" 'find-file
+                 "b" 'switch-to-buffer
+                 "k" 'kill-buffer
+                 "K" 'kill-this-buffer
+                 "s" 'save-buffer
+                 "S" 'evil-write-all
+                 )
+                '()))
+        (evil-mode 1)
+        ,@(if commentary?
+              `((require 'evil-commentary)
+                (evil-commentary-mode))
+              '())
+        ,@(if collection?
+              `((when (require 'evil-collection nil t)
+                  (evil-collection-init)))
+              '())
+        ,@(if surround?
+              `((require 'evil-surround)
+                (global-evil-surround-mode 1))
+              '())
+        )
       #:elisp-packages (list
                         emacs-evil
-                        emacs-evil-commentary
-                        (if evil-collection? emacs-evil-collection)
-                        (if evil-surround? emacs-evil-surround)))))
+                        (if leader? emacs-evil-leader)
+                        (if commentary? emacs-evil-commentary)
+                        (if collection? emacs-evil-collection)
+                        (if surround? emacs-evil-surround)))))
 
   (feature
    (name f-name)
@@ -197,7 +159,7 @@
 (define %engstrand-emacs-base-features
   (list
    (feature-emacs
-    #:emacs emacs-next-pgtk
+    #:emacs emacs-pgtk-native-comp
     #:additional-elisp-packages (list emacs-geiser emacs-geiser-guile)
     #:extra-init-el '((fringe-mode 0)
                       ;; Undo the top modeline of emacs-appearance
@@ -208,6 +170,7 @@
                       (add-hook 'prog-mode-hook 'display-line-numbers-mode)
                       ;; Olivetti mode when working with text
                       (add-hook 'text-mode-hook 'olivetti-mode)
+                      (setq olivetti-set-width 100)
                       ;; Nicer mouse scrolling
                       (setq mouse-wheel-progressive-speed nil)
                       (setq mouse-wheel-scroll-amount '(3))
@@ -215,6 +178,14 @@
                       (setq tab-bar-close-button-show nil
                             tab-bar-new-button-show nil
                             tab-bar-new-tab-choice "*scratch*")
+                      ;; Move to future C feature
+                      (setq c-default-style "linux")
+                      ;; Delete whitespace from indentations immediately
+                      (setq backward-delete-char-untabify-method 'hungry)
+                      ;; Transparency - laggy when window large
+                      ;; (add-to-list 'default-frame-alist '(alpha 93 . 93))
+                      ;; Clean up white space
+                      (add-hook 'before-save-hook 'whitespace-cleanup)
                       ;; Allow execution of src blocks without asking
                       (setq org-confirm-babel-evaluate nil)))
    (feature-emacs-appearance
@@ -225,8 +196,6 @@
    (feature-emacs-faces)
    (feature-emacs-completion
     #:mini-frame? #f)
-   ;; (feature-emacs-auto-complete)
-   ;; (feature-emacs-company)
    (feature-emacs-modus-themes)
    (feature-emacs-corfu)
    (feature-emacs-vertico)
