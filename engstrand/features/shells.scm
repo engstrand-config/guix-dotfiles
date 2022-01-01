260dcfce3c8f16fa1b2ee2861fa1138d04903bdd
Fredrik Engstrand <fredrik@engstrand.nu>
2022-01-01 19:43:00 +0100

    features: shells: revert to rde zsh home-service
    A fresh install using the upstreamed home-service
    had issues with the guix profile not being loaded.
    This seems to work just fine in the old zsh home-
    service in rde, however. There is also some issue
    with the autosuggestion service with the upstreamed
    version.
    
    Co-authored-by: Johan Engstrand <johan@engstrand.nu>

--- engstrand/features/shells.scm
+++ engstrand/features/shells.scm
@@ -1,27 +1,29 @@
 (define-module (engstrand features shells)
   #:use-module (rde features)
+  #:use-module (rde packages)
   #:use-module (rde features predicates)
   #:use-module (guix gexp)
   #:use-module (gnu services)
   #:use-module (gnu packages shells)
-  #:use-module (gnu home services utils)
-  #:use-module (gnu home services shells)
+  #:use-module (gnu home-services-utils)
+  #:use-module (gnu home-services shells)
+  #:use-module (gnu home-services shellutils)
   #:use-module (engstrand utils)
   #:export (feature-zsh))
 
-;; TODO: Add options
 (define* (feature-zsh)
   "Setup zsh."
 
   (define (get-home-services config)
     "Return a list of home services required by zsh"
     (list
-     ;; (service home-zsh-autosuggestions-service-type)
+     (service home-zsh-autosuggestions-service-type
+              zsh-autosuggestions-latest)
      (service home-zsh-service-type
               (home-zsh-configuration
                ;; TODO: Use absolute paths
-               (zshrc `(,(local-file "../files/zshrc")))
-               (zprofile `(,(local-file "../files/shell-profile")))))))
+               (zshrc `(,(slurp-file-gexp (local-file "../files/zshrc"))))
+               (zprofile `(,(slurp-file-gexp (local-file "../files/shell-profile"))))))))
   (feature
    (name 'zsh)
    (values `((login-shell . ,(file-append zsh "/bin/zsh"))))
