6fbc626333a92176858f4e876d67845d72aaf625
Fredrik Engstrand <fredrik@engstrand.nu>
2022-03-20 12:10:27 +0100

    features: laptop: fix: validate battery interface path on reconfigure

--- engstrand/features/laptop.scm
+++ engstrand/features/laptop.scm
@@ -14,13 +14,13 @@
   #:use-module (dtao-guile home-service)
   #:use-module (engstrand utils)
   #:export (
-	    feature-laptop
-	    feature-laptop-tlp
-	    feature-laptop-natural-scrolling
-	    feature-laptop-statusbar-battery
-	    feature-laptop-monitor-brightness
+            feature-laptop
+            feature-laptop-tlp
+            feature-laptop-natural-scrolling
+            feature-laptop-statusbar-battery
+            feature-laptop-monitor-brightness
 
-	    %engstrand-laptop-base-features))
+            %engstrand-laptop-base-features))
 
 (define* (feature-laptop)
   "Base laptop feature."
@@ -30,10 +30,10 @@
    (values `((laptop . #t)))))
 
 (define* (feature-laptop-tlp
-	  #:key
-	  (tlp-config (tlp-configuration
-		       (cpu-scaling-governor-on-ac (list "performance"))
-		       (sched-powersave-on-bat? #t))))
+          #:key
+          (tlp-config (tlp-configuration
+                       (cpu-scaling-governor-on-ac (list "performance"))
+                       (sched-powersave-on-bat? #t))))
   "Setup TLP for power management on laptops."
 
   (define (get-system-services config)
@@ -44,15 +44,15 @@
       profile-service-type
       (pkgs '("tlp")))
      (service tlp-service-type
-	      tlp-config)))
+              tlp-config)))
 
   (feature
    (name 'laptop-tlp)
    (system-services-getter get-system-services)))
 
 (define* (feature-laptop-natural-scrolling
-	  #:key
-	  (natural-scrolling? #t))
+          #:key
+          (natural-scrolling? #t))
   "Enable/disable natural scrolling in compositor."
 
   (ensure-pred boolean? natural-scrolling?)
@@ -61,66 +61,66 @@
     (make-service-list
      (when (get-value 'dwl-guile config)
        (simple-service
-	'set-natural-scrolling-in-dwl-guile
-	home-dwl-guile-service-type
-	(modify-dwl-guile-config
-	 (config =>
-		 (dwl-config
-		  (inherit config)
-		  (natural-scrolling? natural-scrolling?))))))))
+        'set-natural-scrolling-in-dwl-guile
+        home-dwl-guile-service-type
+        (modify-dwl-guile-config
+         (config =>
+                 (dwl-config
+                  (inherit config)
+                  (natural-scrolling? natural-scrolling?))))))))
 
   (feature
    (name 'laptop-natural-scrolling)
    (home-services-getter get-home-services)))
 
 (define* (feature-laptop-statusbar-battery
-	  #:key
-	  (interface "BAT0"))
+          #:key
+          (interface "BAT0"))
   "Add battery indicator to statusbar."
 
   (ensure-pred string? interface)
 
   (define read-path
     (string-append "/sys/class/power_supply/"
-		   interface
-		   "/capacity"))
-
-  (throw-message
-   (not (file-exists? read-path))
-   (string-append "Invalid battery interface name. No such file " read-path))
+                   interface
+                   "/capacity"))
 
   (define (get-home-services config)
+    (throw-message
+     (not (file-exists? read-path))
+     (string-append "Invalid battery interface name. No such file " read-path))
+
     (make-service-list
      (when (get-value 'dtao-guile config)
        (simple-service
-	'add-dtao-guile-battery-block
-	home-dtao-guile-service-type
-	(list
-	 (dtao-block
-	  (position "right")
-	  (interval 10)
-	  (render
-	   `(let* ((port (open-input-file ,read-path))
-		   (result (read-line port))
-		   (percent (string->number result)))
-	      (close-port port)
-	      (string-append "^fg("
-			     (cond
-			      ((<= percent 20) "#ff0000")
-			      ((<= percent 50) "#ffcc00")
-			      (else "#00ff00"))
-			     ")" result "%^fg()")))))))))
+        'add-dtao-guile-battery-block
+        home-dtao-guile-service-type
+        (list
+         (dtao-block
+          (position "right")
+          (interval 10)
+          (render
+           `(let* ((port (open-input-file ,read-path))
+                   (result (read-line port))
+                   (percent (string->number result)))
+              (close-port port)
+              (string-append "^fg("
+                             (cond
+                              ((<= percent 20) "#ff0000")
+                              ((<= percent 50) "#ffcc00")
+                              (else "#00ff00"))
+                             ")" result "%^fg()")))))))))
 
   (feature
    (name 'laptop-statusbar-battery)
    (home-services-getter get-home-services)))
 
 (define* (feature-laptop-monitor-brightness
-	  #:key
-	  (step 10)
-	  (decrease-brightness-key "<XF86MonBrightnessDown>")
-	  (increase-brightness-key "<XF86MonBrightnessUp>")
-	  (add-keybindings? #t))
+          #:key
+          (step 10)
+          (decrease-brightness-key "<XF86MonBrightnessDown>")
+          (increase-brightness-key "<XF86MonBrightnessUp>")
+          (add-keybindings? #t))
   "Install and configure brightnessctl for laptops"
 
   (ensure-pred number? step)
@@ -135,26 +135,26 @@
       home-profile-service-type
       (list brightnessctl))
      (when (and add-keybindings?
-		(get-value 'dwl-guile config))
+                (get-value 'dwl-guile config))
        (let ((bin (file-append brightnessctl "/bin/brightnessctl"))
-	     (change (string-append (number->string step) "%")))
-	 (simple-service
-	  'add-dwl-guile-brightness-keys
-	  home-dwl-guile-service-type
-	  (modify-dwl-guile-config
-	   (config =>
-		   (dwl-config
-		    (inherit config)
-		    (keys
-		     (append
-		      (list
-		       (dwl-key
-			(key decrease-brightness-key)
-			(action `(system* ,bin "s" ,(string-append change "-"))))
-		       (dwl-key
-			(key increase-brightness-key)
-			(action `(system* ,bin "s" ,(string-append "+" change)))))
-		      (dwl-config-keys config)))))))))))
+             (change (string-append (number->string step) "%")))
+         (simple-service
+          'add-dwl-guile-brightness-keys
+          home-dwl-guile-service-type
+          (modify-dwl-guile-config
+           (config =>
+                   (dwl-config
+                    (inherit config)
+                    (keys
+                     (append
+                      (list
+                       (dwl-key
+                        (key decrease-brightness-key)
+                        (action `(system* ,bin "s" ,(string-append change "-"))))
+                       (dwl-key
+                        (key increase-brightness-key)
+                        (action `(system* ,bin "s" ,(string-append "+" change)))))
+                      (dwl-config-keys config)))))))))))
 
   (feature
    (name 'laptop-monitor-brightness)
