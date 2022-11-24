bf785902d3b8d9e680c914f4017f85acea7e683c
Fredrik Engstrand <fredrik@engstrand.nu>
2022-11-24 16:50:20 +0100

    features: statusbar: use farg in dtao blocks

--- engstrand/features/statusbar.scm
+++ engstrand/features/statusbar.scm
@@ -5,16 +5,18 @@
   #:use-module (rde features)
   #:use-module (rde features predicates)
   #:use-module (engstrand utils)
+  #:use-module (farg colorscheme)
   #:use-module (dtao-guile home-service)
   #:export (
             feature-statusbar-dtao-guile
 
-            %engstrand-dtao-guile-config
-            %engstrand-dtao-guile-left-blocks
-            %engstrand-dtao-guile-center-blocks
-            %engstrand-dtao-guile-right-blocks))
+            engstrand-dtao-guile-left-blocks
+            engstrand-dtao-guile-center-blocks
+            engstrand-dtao-guile-right-blocks
 
-(define %engstrand-dtao-guile-left-blocks
+            %engstrand-dtao-guile-config))
+
+(define (engstrand-dtao-guile-left-blocks palette)
   (append
    (map
     (lambda (tag)
@@ -27,11 +29,20 @@
                    (0 (dtao:view ,index))))
          (render `(cond
                    ((dtao:selected-tag? ,index)
-                    ,(string-append "^bg(#ffcc00)^fg(#191919)" str "^fg()^bg()"))
+                    ,(format #f "^bg(~a)^fg(~a)~a^fg()^bg()"
+                             (palette 'primary)
+                             (palette 'background)
+                             str))
                    ((dtao:urgent-tag? ,index)
-                    ,(string-append "^bg(#ff0000)^fg(#ffffff)" str "^fg()^bg()"))
+                    ,(format #f "^bg(~a)^fg(~a)~a^fg()^bg()"
+                             "#FF0000" ;; TODO: Use red from palette
+                             (palette 'background) ;; TODO: Light/dark based on theme mode
+                             str))
                    ((dtao:active-tag? ,index)
-                    ,(string-append "^bg(#323232)^fg(#ffffff)" str "^fg()^bg()"))
+                    ,(format #f "^bg(~a)^fg(~a)~a^fg()^bg()"
+                             (offset (palette 'background) 10)
+                             (palette 'text)
+                             str))
                    (else ,str))))))
     (iota 9 1))
    (list
@@ -40,13 +51,13 @@
      (click `(dtao:next-layout))
      (render `(string-append "^p(4)" (dtao:get-layout)))))))
 
-(define %engstrand-dtao-guile-center-blocks
+(define (engstrand-dtao-guile-center-blocks palette)
   (list
    (dtao-block
     (events? #t)
     (render `(dtao:title)))))
 
-(define %engstrand-dtao-guile-right-blocks
+(define (engstrand-dtao-guile-right-blocks palette)
   (list
    (dtao-block
     (interval 1)
@@ -59,33 +70,34 @@
   ;; Statusbar height
   (define height 25)
 
-  (define (get-home-services config)
-    "Return a list of home services required by dtao-guile."
-    (require-value 'font-monospace config)
-    (list
-     (service home-dtao-guile-service-type
-              (home-dtao-guile-configuration
-               (config
-                (dtao-config
-                 (font (font->string 'fcft 'font-monospace config
-                                     #:bold? #t))
-                 (block-spacing 0)
-                 (use-dwl-guile-colorscheme? #t)
-                 (modules '((ice-9 match)
-                            (ice-9 popen)
-                            (ice-9 rdelim)
-                            (srfi srfi-1)))
-                 (padding-left 0)
-                 (padding-top 0)
-                 (padding-bottom 0)
-                 (height height)
-                 (left-blocks %engstrand-dtao-guile-left-blocks)
-                 (center-blocks %engstrand-dtao-guile-center-blocks)
-                 (right-blocks %engstrand-dtao-guile-right-blocks)))))))
+  (lambda (fconfig palette)
+    (define (get-home-services config)
+      "Return a list of home services required by dtao-guile."
+      (require-value 'font-monospace config)
+      (list
+       (service home-dtao-guile-service-type
+                (home-dtao-guile-configuration
+                 (config
+                  (dtao-config
+                   (font (font->string 'fcft 'font-monospace config
+                                       #:bold? #t))
+                   (block-spacing 0)
+                   (use-dwl-guile-colorscheme? #t)
+                   (modules '((ice-9 match)
+                              (ice-9 popen)
+                              (ice-9 rdelim)
+                              (srfi srfi-1)))
+                   (padding-left 0)
+                   (padding-top 0)
+                   (padding-bottom 0)
+                   (height height)
+                   (left-blocks (engstrand-dtao-guile-left-blocks palette))
+                   (center-blocks (engstrand-dtao-guile-center-blocks palette))
+                   (right-blocks (engstrand-dtao-guile-right-blocks palette))))))))
 
-  (feature
-   (name 'statusbar-dtao-guile)
-   (values `((statusbar? . #t)
-             (statusbar-height . ,height)
-             (dtao-guile . #t)))
-   (home-services-getter get-home-services)))
+      (feature
+       (name 'statusbar-dtao-guile)
+       (values `((statusbar? . #t)
+                 (statusbar-height . ,height)
+                 (dtao-guile . #t)))
+       (home-services-getter get-home-services))))
