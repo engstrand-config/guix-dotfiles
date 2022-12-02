54e051678052e4d1421236d1170b048b6a3f1b59
Fredrik Engstrand <fredrik@engstrand.nu>
2022-12-02 13:41:08 +0100

    features: utils: use upstreamed rbw package

--- engstrand/features/utils.scm
+++ engstrand/features/utils.scm
@@ -6,11 +6,10 @@
   #:use-module (gnu services)
   #:use-module (gnu services dbus)
   #:use-module (gnu packages gnome)
+  #:use-module (gnu packages rust-apps)
   #:use-module (gnu home services)
   #:use-module (gnu home services shepherd)
   #:use-module (engstrand utils)
-  #:use-module (engstrand packages rust)
-  #:use-module (engstrand packages utils)
   #:export (feature-imv
             feature-bitwarden-cli
             feature-piper))
@@ -70,52 +69,50 @@
 
   (define (get-home-services config)
     "Return a list of home services required by rbw"
-    (list))
-    ;; TODO: rbw package is broken because of rust packages removed from upstream
-    ;; (list
-    ;;  (simple-service
-    ;;   'add-rbw-home-packages-to-profile
-    ;;   home-profile-service-type
-    ;;   (list rbw))
-    ;;  ;; rbw mutates the configuration file to set a unique device-id
-    ;;  ;; upon registering the device using your API key.
-    ;;  ;; See https://github.com/doy/rbw/issues/74.
-    ;;  ;;
-    ;;  ;; To circumvent this, a separate config file is saved, allowing us
-    ;;  ;; to listen for changes and mutate the real config file.
-    ;;  (simple-service
-    ;;   'create-rbw-config
-    ;;   home-files-service-type
-    ;;   `((".config/rbw/guix-watcher"
-    ;;      ,(mixed-text-file
-    ;;        "guix-watcher"
-    ;;        email (number->string lock-timeout) pinentry))))
-    ;;  (simple-service
-    ;;   'on-rbw-config-change
-    ;;   home-run-on-change-service-type
-    ;;   (let ((bin (file-append rbw "/bin/rbw")))
-    ;;     `(("files/config/rbw/guix-watcher"
-    ;;        ,#~(begin
-    ;;             (when #$email
-    ;;               (system* #$bin "config" "set" "email" #$email))
-    ;;             (system* #$bin "config" "set" "pinentry"
-    ;;                      #$(file-append pinentry "/bin/pinentry-gtk-2"))
-    ;;             (system* #$bin "config" "set" "lock_timeout"
-    ;;                      #$(number->string lock-timeout)))))))
-    ;;  (simple-service
-    ;;   'add-rbw-initial-setup-shepherd-service
-    ;;   home-shepherd-service-type
-    ;;   (list
-    ;;    (shepherd-service
-    ;;     (documentation "Run initial setup for rbw (Bitwarden CLI).")
-    ;;     (provision '(rbw-setup))
-    ;;     (requirement '())
-    ;;     (auto-start? #f)
-    ;;     (respawn? #f)
-    ;;     (start
-    ;;      #~(make-forkexec-constructor
-    ;;         (list #$(file-append rbw "/bin/rbw") "register")))
-    ;;     (stop #~(make-kill-destructor)))))))
+    (list
+     (simple-service
+      'add-rbw-home-packages-to-profile
+      home-profile-service-type
+      (list rbw))
+     ;; rbw mutates the configuration file to set a unique device-id
+     ;; upon registering the device using your API key.
+     ;; See https://github.com/doy/rbw/issues/74.
+     ;;
+     ;; To circumvent this, a separate config file is saved, allowing us
+     ;; to listen for changes and mutate the real config file.
+     (simple-service
+      'create-rbw-config
+      home-files-service-type
+      `((".config/rbw/guix-watcher"
+         ,(mixed-text-file
+           "guix-watcher"
+           email (number->string lock-timeout) pinentry))))
+     (simple-service
+      'on-rbw-config-change
+      home-run-on-change-service-type
+      (let ((bin (file-append rbw "/bin/rbw")))
+        `(("files/config/rbw/guix-watcher"
+           ,#~(begin
+                (when #$email
+                  (system* #$bin "config" "set" "email" #$email))
+                (system* #$bin "config" "set" "pinentry"
+                         #$(file-append pinentry "/bin/pinentry-gtk-2"))
+                (system* #$bin "config" "set" "lock_timeout"
+                         #$(number->string lock-timeout)))))))
+     (simple-service
+      'add-rbw-initial-setup-shepherd-service
+      home-shepherd-service-type
+      (list
+       (shepherd-service
+        (documentation "Run initial setup for rbw (Bitwarden CLI).")
+        (provision '(rbw-setup))
+        (requirement '())
+        (auto-start? #f)
+        (respawn? #f)
+        (start
+         #~(make-forkexec-constructor
+            (list #$(file-append rbw "/bin/rbw") "register")))
+        (stop #~(make-kill-destructor)))))))
 
   (feature
    (name 'bitwarden-cli)
