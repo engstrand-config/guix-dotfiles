e57b64fd0a766d498053dc70eae3bc777cccfdaf
Fredrik Engstrand <fredrik@engstrand.nu>
2023-01-26 11:01:40 +0100

    features: virtualization: add docker feature

--- engstrand/features/virtualization.scm
+++ engstrand/features/virtualization.scm
@@ -2,9 +2,10 @@
   #:use-module (rde features)
   #:use-module (rde features predicates)
   #:use-module (gnu services)
+  #:use-module (gnu services docker)
   #:use-module (gnu services virtualization)
   #:use-module (rde system services accounts)
-  #:export (feature-virtualization))
+  #:export (feature-virtualization feature-docker))
 
 (define* (feature-virtualization
           #:key
@@ -27,4 +28,20 @@
 
   (feature
    (name 'virtualization)
+   (system-services-getter get-system-services)))
+
+(define* (feature-docker)
+  "Setup docker."
+
+  (define (get-system-services config)
+    "Return a list of system services required for docker."
+    (list
+     (simple-service
+      'docker-add-user-groups
+      rde-account-service-type
+      (list "docker"))
+     (service docker-service-type)))
+
+  (feature
+   (name 'docker)
    (system-services-getter get-system-services)))
