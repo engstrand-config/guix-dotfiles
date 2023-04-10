7504446a4ea9af8a328f199068229f44ad1c74f2
Fredrik Engstrand <fredrik@engstrand.nu>
2023-04-10 08:58:40 +0200

    features: bluetooth: add feature for bluetooth quick connect

--- engstrand/features/bluetooth.scm
+++ engstrand/features/bluetooth.scm
@@ -1,28 +1,28 @@
 (define-module (engstrand features bluetooth)
   #:use-module (rde features)
   #:use-module (rde features predicates)
-  #:use-module (gnu packages linux)
   #:use-module (gnu services)
-  #:use-module (gnu services desktop)
+  #:use-module (dwl-guile home-service)
   #:use-module (engstrand utils)
-  #:use-module (rde system services accounts)
-  #:export (feature-bluetooth))
+  #:export (feature-bluetooth-quick-connect))
 
-(define* (feature-bluetooth
+(define* (feature-bluetooth-quick-connect
           #:key
-          (auto-enable? #t))
-  "Configure and run the bluetoothd daemon."
+          (key "S-s-b"))
+  "Add keybinding for quickly connecting to bluetooth devices."
 
-  (define (get-system-services config)
+  (define (get-home-services config)
     (list
-     (simple-service
-      'bluetooth-add-user-groups
-      rde-account-service-type
-      (list "lp"))
-     (bluetooth-service
-      #:bluez bluez
-      #:auto-enable? auto-enable?)))
+     (when (get-value 'dwl-guile config)
+       (simple-service
+        'bluetooth-dwl-guile-add-keybindings
+        home-dwl-guile-service-type
+        `((set-keys ,key
+                    (lambda ()
+                      ;; emacsclient prevents bluetooth from powering off.
+                      (dwl:spawn "emacsclient" "-c"
+                                 "--eval" "(bluetooth-list-devices)"))))))))
 
   (feature
-   (name 'bluetooth)
-   (system-services-getter get-system-services)))
+   (name 'bluetooth-quick-connect)
+   (home-services-getter get-home-services)))
