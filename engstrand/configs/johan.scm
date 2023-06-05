c489b6b455c50677481d1567ecc1d28f9afb9051
Johan Engstrand <johan@engstrand.nu>
2023-06-05 15:49:29 +0200

    configs: johan: update user features

--- engstrand/configs/johan.scm
+++ engstrand/configs/johan.scm
@@ -3,13 +3,15 @@
   #:use-module (gnu services)
   #:use-module (rde features base)
   #:use-module (rde features gnupg)
-  #:use-module (rde features bittorrent)
+  #:use-module (rde features web-browsers)
   #:use-module (rde features mail)
   #:use-module (farg source pywal)
   #:use-module (engstrand utils)
   #:use-module (engstrand configs)
+  #:use-module (engstrand themes)
   #:use-module (engstrand wallpapers) ;; get-wallpaper-path
   #:use-module (engstrand features daw)
+  #:use-module (engstrand features documents)
   #:use-module (engstrand features sync)
   #:use-module (engstrand features emacs)
   #:use-module (engstrand features utils)
@@ -18,10 +20,7 @@
   #:use-module (engstrand features browsers)
   #:use-module (engstrand features virtualization))
 
-(define-public %user-theme
-  (farg:generator-pywal (get-wallpaper-path "nature/jack-b-8Wqm1W59Baw-unsplash.jpg")
-                        #:light? #f
-                        #:alpha 0.96))
+(define-public %user-theme engstrand-theme-dark)
 
 (define email-engstrand-primary "johan@engstrand.nu")
 
@@ -37,12 +36,7 @@
      #:pinentry-flavor 'gtk2)
     (feature-virtualization)
     (feature-kdeconnect)
-    (feature-transmission
-     #:download-dir "~/downloads")
     (feature-piper)
-    (feature-ardour)
-    (feature-calf-plugins)
-    ;; (feature-sfz)
     (feature-mail-settings
      #:mail-accounts (list (mail-account (id 'personal) (fqda email-engstrand-primary) (type 'generic))))
     (feature-emacs-message)
@@ -54,7 +48,8 @@
                    (tls_starttls . on)))))
     (feature-isync #:isync-verbose #t)
     (feature-l2md)
-    (feature-qutebrowser)
+    (feature-sioyek)
+    (feature-ungoogled-chromium)
     (feature-firefox
      #:default-browser? #t))
    %engstrand-emacs-base-features
